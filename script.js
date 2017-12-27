b32d329fc9f26d21517df22c1306297e972d129e
Tyler Fuqua <tyler@agilityda.com>
2017-12-26 21:51:52 -0700

    Hands On Project 2

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 // Hands On Project 1
 
 
-function phoneNumberFormat(phoneNumber) {
-    phoneNumber = document.getElementById("phNumber").value;
-    let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/g;
-    // Convert the following if..esle into a ternary
-    // condition ? Expression on a true : false
-    phoneNumber.match(phoneRegex) <= phoneRegex
-    ? console.log("Valid number entered")
-    : console.log( "Phone number entered is invalid");
+// function phoneNumberFormat(phoneNumber) {
+//     phoneNumber = document.getElementById("phNumber").value;
+//     let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/g;
+//     // Convert the following if..esle into a ternary
+//     // condition ? Expression on a true : false
+//     phoneNumber.match(phoneRegex) <= phoneRegex
+//     ? console.log("Valid number entered")
+//     : console.log( "Phone number entered is invalid");
     
-}
-    
-
+// }
 //     if (phoneNumber.match(phoneRegex)) {
 //       alert("Valid number entered");
 //     } else {
 //       alert("Phone number entered is invalid");
 //     }
 //   }
+
+// Hands On Project 2
+// Write a try...catch statement using the given variable; 
+// it should have at least three try statements such as if (x < 5) throw true. 
+// Your catch should log error to the console. 
+
+let newNumber = 10;
+
+try {
+    if (newNumber > 5) 
+    console.log(false);
+    if (newNumber < 5) 
+    console.log(true);
+    if (newNumber == 5)
+    console.log("Not equal");
+} catch (err) {
+    console.log(err);
+}
