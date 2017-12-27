3e85641596de5ce638dc3bdf3b1d6001eb997703
Tyler Fuqua <tyler@agilityda.com>
2017-12-27 07:51:22 -0700

    Hands On Project 3

--- script.js
+++ script.js
@@ -23,15 +23,35 @@
 // it should have at least three try statements such as if (x < 5) throw true. 
 // Your catch should log error to the console. 
 
-let newNumber = 10;
+// let newNumber = 10;
 
-try {
-    if (newNumber > 5) 
-    console.log(false);
-    if (newNumber < 5) 
-    console.log(true);
-    if (newNumber == 5)
-    console.log("Not equal");
-} catch (err) {
-    console.log(err);
+// try {
+//     if (newNumber > 5) 
+//     console.log(false);
+//     if (newNumber < 5) 
+//     console.log(true);
+//     if (newNumber == 5)
+//     console.log("Not equal");
+// } catch (err) {
+//     console.log(err);
+// }
+
+// Hands On Project 3
+// Write a function that checks the input of a user's name for the proper format. 
+// The first letter of both first name and last names should be capitalized. 
+// The user must more than one characters for first name and last name.
+// No special characters should be available. 
+
+function fullNameFormat(firstName, lastName) {
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    let nameRegex = /^[A-Z][a-zA-Z]*$/;
+    // Convert the following if..esle into a ternary
+    // condition ? Expression on a true : false
+    firstName.match(nameRegex) == firstName
+    ? console.log("Valid name")
+    : console.log( "Name entered is invalid");
+    lastName.match(nameRegex) == lastName
+    ? console.log("Valid name")
+    : console.log( "Name entered is invalid");
 }
